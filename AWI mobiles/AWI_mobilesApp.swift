77cab13a81b221128831f216ac8cfe1ede3a0d3b
axel.laget <axel.laget@gmail.com>
2022-02-18 12:03:38 +0100

    Initial Commit

---
+++ AWI mobiles/AWI_mobilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWI_mobilesApp.swift
+//  AWI mobiles
+//
+//  Created by m1 on 18/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct AWI_mobilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AWI mobilesUITests/AWI_mobilesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AWI_mobilesUITestsLaunchTests.swift
+//  AWI mobilesUITests
+//
+//  Created by m1 on 18/02/2022.
+//
+
+import XCTest
+
+class AWI_mobilesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
